2ac1497cff35e85cdc68f5024c0d040c73e5d17c
dars2002 <u20191e831@upc.edu.pe>
2024-11-19 17:23:57 -0500

    fix: issues service.

--- src/app/issue/services/issues.service.ts
+++ src/app/issue/services/issues.service.ts
@@ -12,37 +12,50 @@ export class IssuesService extends BaseService<Issue> {
     super(httpClient);
     this.resourceEndpoint = '/issues';
   }
-  protected httpOptions = {
-    headers: new HttpHeaders({
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${localStorage.getItem('token') || ''}`, // Incluye el token si existe
-    }),
-  };
+  private createHttpOptions() {
+      const token = localStorage.getItem('token'); // Obtener el token del localStorage
+      return {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+          'Authorization': token ? `Bearer ${token}` : '' // Agregar el token en la cabecera si existe
+        })
+      };
+    }
   // Nuevo método específico para IssuesService
   getAllIssues(): Observable<Issue[]> {
-    return this.httpClient.get<Issue[]>(`${this.basePath}${this.resourceEndpoint}`, this.httpOptions);
+        const httpOptions = this.createHttpOptions();
+
+    return this.httpClient.get<Issue[]>(`${this.basePath}${this.resourceEndpoint}`, httpOptions);
   }
 
    deleteIssue(id: number): Observable<void> {
-         return this.httpClient.delete<void>(`${this.basePath}${this.resourceEndpoint}/${id}`, this.httpOptions);
+         const httpOptions = this.createHttpOptions();
+
+         return this.httpClient.delete<void>(`${this.basePath}${this.resourceEndpoint}/${id}`, httpOptions);
        }
 
   // Método específico para actualizar un issue existente
     override update(id: number, issue: Issue): Observable<Issue> {
-      return this.httpClient.put<Issue>(`${this.basePath}${this.resourceEndpoint}/${id}`, issue, this.httpOptions);
+          const httpOptions = this.createHttpOptions();
+
+      return this.httpClient.put<Issue>(`${this.basePath}${this.resourceEndpoint}/${id}`, issue, httpOptions);
     }
 
   // Método específico para crear un nuevo issue
     createIssue(issue: Issue): Observable<Issue> {
-      return this.httpClient.post<Issue>(`${this.basePath}${this.resourceEndpoint}`, issue, this.httpOptions);
+          const httpOptions = this.createHttpOptions();
+
+      return this.httpClient.post<Issue>(`${this.basePath}${this.resourceEndpoint}`, issue, httpOptions);
     }
 
    // Método específico para añadir un evento de historial a un issue dado
     addHistoryEventToIssue(issueId: number, historyEvent: History): Observable<History> {
+          const httpOptions = this.createHttpOptions();
+
       return this.httpClient.post<History>(
         `${this.basePath}${this.resourceEndpoint}/${issueId}/events`,
         historyEvent,
-        this.httpOptions
+        httpOptions
       );
     }
 }
